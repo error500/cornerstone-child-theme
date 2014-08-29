c389873a2dd82d5691f35b7e82caa81e47191e06
error500 <error500@free.fr>
2014-08-29 07:55:41 +0200

    removed loading orbit functionnality

--- functions.php
+++ functions.php
@@ -16,21 +16,31 @@ if ( ! function_exists( 'load_cornerstone_child_scripts' ) ) {
 	
 	function load_cornerstone_child_scripts() {
 
-		wp_enqueue_script(
+		/*wp_enqueue_script(
 			'foundation_interchange_js',
 			get_template_directory_uri() . '/libs/foundation/js/foundation/foundation.interchange.js',
 			array('foundation_js'),
 			'1.0',
 			true
-		);
+		);*/
 		// load if needed
-		wp_enqueue_script(
+
+		// Orbit is disable since it is deprecated. Replaced with owl loaded in parent cornerstone theme
+		/*wp_enqueue_script(
 			'foundation_orbit_js',
 			get_template_directory_uri() . '/libs/foundation/js/foundation/foundation.orbit.js',
 			array('foundation_js'),
 			'1.0',
 			true
-		);
+		);*/
+		//Load your own js file if needed
+		/*wp_enqueue_script(
+			'child_js',
+			get_stylesheet_directory_uri(). '/js/child.js',
+			array( 'jquery' ),
+			'1.0',
+			true
+		);	*/
 	}
 }
 
